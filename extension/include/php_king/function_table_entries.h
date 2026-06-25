bbc527869a8d2e3d8a70ef53c033ad0fda06e275
Jochen Schultz <jschultz@php.net>
2026-06-25 13:05:41 +0200

    refactor: aggregate module state storage

--- extension/include/php_king/function_table_entries.h
+++ extension/include/php_king/function_table_entries.h
@@ -8,30 +8,30 @@ static const zend_function_entry king_functions[] = {
     PHP_FE(king_health, arginfo_king_no_args)
     PHP_FE(king_get_last_error, arginfo_king_no_args)
     PHP_FE(king_new_config, arginfo_king_new_config)
-#include "../awaitable/function_entries.h"
-#include "../client/function_entries.h"
-#include "../db_ingest/function_entries.h"
-#include "../server/function_entries.h"
-#include "../media/function_entries.h"
+#include "awaitable/function_entries.h"
+#include "client/function_entries.h"
+#include "db_ingest/function_entries.h"
+#include "server/function_entries.h"
+#include "media/function_entries.h"
 
-#include "../iibin/function_entries.h"
+#include "iibin/function_entries.h"
 
-#include "../mcp/function_entries.h"
+#include "mcp/function_entries.h"
 
-#include "../pipeline_orchestrator/function_entries.h"
+#include "pipeline_orchestrator/function_entries.h"
 
-#include "../semantic_dns/function_entries.h"
+#include "semantic_dns/function_entries.h"
 
-#include "../object_store/function_entries.h"
+#include "object_store/function_entries.h"
 
-#include "../xslt/function_entries.h"
-#include "../inference/function_entries.h"
+#include "xslt/function_entries.h"
+#include "inference/function_entries.h"
 
-#include "../telemetry/function_entries.h"
+#include "telemetry/function_entries.h"
 
-#include "../autoscaling/function_entries.h"
+#include "autoscaling/function_entries.h"
 
-#include "../integration/function_entries.h"
+#include "integration/function_entries.h"
 
     PHP_FE_END
 };
